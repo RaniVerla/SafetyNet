00803a228b5fb69d171620ef13ff4625d2e0fdfb
raniv <rani.verla@gmail.com>
2026-01-21 16:43:23 -0800

    replaced the spring-mvc dependency with springboot web  dependency

--- src/main/java/net/example/safetynet/controller/TestController.java
+++ src/main/java/net/example/safetynet/controller/TestController.java
@@ -1,10 +1,13 @@
 package net.example.safetynet.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RequestMapping("/safetynet")
 @RestController
 public class TestController {
@@ -12,6 +15,7 @@ public class TestController {
     @GetMapping("/test")
     public HttpStatus testReturn()
     {
+        log.info("Testing Person Get method");
         return HttpStatus.OK;
     }
     
